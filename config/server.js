f63ed049d46bd46d58461eb7038cd59b8acb2a8b
bima-shatyakidutt <shatyaki.dutt@milvik.se>
2021-06-18 22:46:36 +0530

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ce4488c13cafd648567ade88b5a76162'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cd5fbf73-2210-40f4-a73c-f76b920e36cd'
+};
